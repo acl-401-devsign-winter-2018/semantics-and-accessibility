e337b256ef31ca12128e3f45cf8ee42baf6a03af
Ivan Limongan <ivan.limongan@gmail.com>
2018-01-09 10:37:00 -0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     new HtmlPlugin({ template: `./src/a-new-hope.html`, filename: `a-new-hope.html`}),
     new HtmlPlugin({ template: `./src/empire-strikes-back.html`, filename: `empire-strikes-back.html`}),
     new HtmlPlugin({ template: `./src/return-of-jedi.html`, filename: `return-of-jedi.html`}),
-    new CopyWebpackPlugin([{ from: 'src/images/ivan-logo.png', to: 'ivan-logo.png' }])
+    new CopyWebpackPlugin([{ from: 'src/images/Star_wars_1977_us.svg.png', to: 'Star_wars_1977_us.svg.png' }])
   ],
   module: {
     rules: [
