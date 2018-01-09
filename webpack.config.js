2d86783c407be66c3816b0e0ed8b3b54b563bb17
Zac-L <Zachlichens@gmail.com>
2018-01-09 09:27:44 -0800

    updated to include CopyWebpackPlugin, added three ultra basic html files to make sure everything is working. added images folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: `./src/main.js`,
   plugins: [
     new HtmlPlugin({ template: `./src/index.html` }),
+    new HtmlPlugin({ template: `./src/second-file.html`, filename: `second-file.html` }),
+    new HtmlPlugin({ template: `./src/third-file.html`, filename: `third-file.html` }),
+    new CopyWebpackPlugin([
+      {from: 'src/images', to: 'images'}
+    ])
   ],
   module: {
     rules: [
@@ -15,8 +21,8 @@ module.exports = {
             interpolate: true,
             attrs: false
           }
-        }
-      }      
+        },
+      }     
     ]
   }
 };
