90b1bd9dedde42c583ffa2bcaeef2cb65f73e32c
Ivan Limongan <ivan.limongan@gmail.com>
2018-01-09 09:45:59 -0800

    installed copywebpackplugin, image working now

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: `./src/main.js`,
@@ -6,7 +7,8 @@ module.exports = {
     new HtmlPlugin({ template: `./src/index.html` }),
     new HtmlPlugin({ template: `./src/a-new-hope.html`, filename: `a-new-hope.html`}),
     new HtmlPlugin({ template: `./src/empire-strikes-back.html`, filename: `empire-strikes-back.html`}),
-    new HtmlPlugin({ template: `./src/return-of-jedi.html`, filename: `return-of-jedi.html`})
+    new HtmlPlugin({ template: `./src/return-of-jedi.html`, filename: `return-of-jedi.html`}),
+    new CopyWebpackPlugin([{ from: 'src/images/ivan-logo.png', to: 'ivan-logo.png' }])
   ],
   module: {
     rules: [
