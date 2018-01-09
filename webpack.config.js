31b27241fca1073e68d7af90216f4e455211b3c5
Ivan Limongan <ivan.limongan@gmail.com>
2018-01-08 17:20:30 -0800

    added pages, and first article from wikipedia

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
   entry: `./src/main.js`,
   plugins: [
     new HtmlPlugin({ template: `./src/index.html` }),
+    new HtmlPlugin({ template: `./src/a-new-hope.html`, filename: `a-new-hope.html`}),
+    new HtmlPlugin({ template: `./src/empire-strikes-back.html`, filename: `empire-strikes-back.html`}),
+    new HtmlPlugin({ template: `./src/return-of-jedi.html`, filename: `return-of-jedi.html`})
   ],
   module: {
     rules: [
