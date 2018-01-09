b0abeaee98ac23d012f225a84402b9755e06d287
ckrountree <jmail0207@gmail.com>
2018-01-09 11:10:31 -0800

    add HtmlPlugins to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: `./src/main.js`,
+	output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
 	plugins: [
 		new HtmlPlugin({ template: `./src/index.html` }),
-		new HtmlPlugin({ template: `./src/second-file.html`, filename: `second-file.html` }),
-		new HtmlPlugin({ template: `./src/third-file.html`, filename: `third-file.html` }),
+		// new HtmlPlugin({ template: `./src/toc.html`, filename: `toc.html` }),
+		new HtmlPlugin({ template: `./src/article1.html`, filename: `article1.html` }),
 	],
 	module: {
 		rules: [
