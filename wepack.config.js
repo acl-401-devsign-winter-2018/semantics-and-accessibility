86322ece4c20d3bb9f4de710e69a4d1a9926cefb
ckrountree <jmail0207@gmail.com>
2018-01-08 21:55:06 -0800

    add webpack.config.js

---
+++ wepack.config.js
@@ -0,0 +1,24 @@
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: `./src/main.js`,
+	plugins: [
+		new HtmlPlugin({ template: `./src/index.html` }),
+		new HtmlPlugin({ template: `./src/second-file.html`, filename: `second-file.html` }),
+		new HtmlPlugin({ template: `./src/third-file.html`, filename: `third-file.html` }),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(html)$/,
+				use: {
+					loader: 'html-loader',
+					options: {
+						interpolate: true,
+						attrs: false
+					}
+				}
+			}     
+		]
+	}
+};
