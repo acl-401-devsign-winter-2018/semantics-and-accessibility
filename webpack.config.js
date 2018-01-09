3ad7692896dce6f0a8a757c792f81e97c269c095
erdem koral <koral.erdem@gmail.com>
2018-01-09 09:09:13 -0800

    add app and index files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: `./src/main.js`,
+  plugins: [
+    new HtmlPlugin({ template: `./src/index.html` }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            interpolate: true,
+            attrs: false
+          }
+        }
+      }      
+    ]
+  }
+}
