1710bc8c6c2d2cb342cc36d2e53553872b7ae0be
mugworks <greenwood.pdx@gmail.com>
2018-01-09 15:07:00 -0800

    add more to article2

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     new HtmlPlugin({ template: `./src/index.html` }),
     new HtmlPlugin({ template: `./src/article1.html`, filename: `article1.html` }),
-    // new HtmlPlugin({ template: `./src/third-file.html`, filename: `third-file.html` }),
+    new HtmlPlugin({ template: `./src/article2.html`, filename: `article2.html` }),
     new CopyWebpackPlugin([
       {from: 'src/images', to: 'images'}
     ])
