3d4a93128886d20a87f883ff1ea50e51d365153d
mugworks <greenwood.pdx@gmail.com>
2018-01-09 17:12:37 -0800

    add webpack config and initial css

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,18 @@ module.exports = {
             attrs: false
           }
         }
-      }      
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1, }
+          },
+          'postcss-loader'
+        ]
+      }
     ]
   }
 };
